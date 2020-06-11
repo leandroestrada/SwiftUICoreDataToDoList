5adcfda562a1574d9f383ca75f1052ed7208ca3a
leandro <leandro.estrada@itsgroup.com.br>
2020-06-11 11:43:55 -0300

    Initial Commit

---
+++ SwiftUiCoreDataToDoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiCoreDataToDoList
+//
+//  Created by leandro de araujo estrada on 11/06/20.
+//  Copyright © 2020 leandro de araujo estrada. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
